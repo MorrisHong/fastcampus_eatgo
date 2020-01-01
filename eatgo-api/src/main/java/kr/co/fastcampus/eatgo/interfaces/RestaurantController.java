e55e8c9f4cce1e6d4a78db55a14ef4b7e9c991f2
gracelove <govlmo91@gmail.com>
2020-01-01 15:21:49 +0900

    가게상세 2

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,35 +1,27 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.Restaurant;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantController {
 
+    private final RestaurantRepository restaurantRepository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
-        restaurants.add(restaurant);
-
+        List<Restaurant> restaurants = restaurantRepository.findAll();
         return restaurants;
     }
 
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable Long id) {
-        List<Restaurant> restaurants = new ArrayList<>();
-        restaurants.add(new Restaurant(1004L,"Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L,"Cyber Food", "Seoul"));
-
-        return restaurants.stream().filter(r -> r.getId().equals(id)).findFirst().orElseThrow();
+        return restaurantRepository.findById(id);
     }
 }
